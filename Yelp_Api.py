dc87a5f3bb5f0b44755f8c676fa97c15f4d4c2c8
Mwansa Puta <mwansa.puta@my.metrostate.edu>
2021-07-07 21:28:54 +0000

    First Push
    Co-authored-by: Melanie Jimenez <mmjimenez@owu.edu>

--- Yelp_Api.py
+++ Yelp_Api.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px 
 
 
 #  collect input
@@ -33,7 +35,6 @@ def collect_liked_input():
    return food_type_liked
 
 
-   
 # SQL, save database, save to file, load file, load database    
 def saveSQLtoFile(database_name, file_name):
    os.system('mysqldump -u root -pcodio '+database_name+' > '+ file_name)
@@ -43,61 +44,90 @@ def loadSQLfromFile(database_name, file_name):
    os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS '+database_name+';"')
    os.system("mysql -u root -pcodio "+database_name+" < " + file_name)
    
+   
 def saveDatasetToFile(database_name, file_name, dataframe, table_name):
    engine= create_engine('mysql://root:codio@localhost/'+database_name+'?charset=utf8', encoding='utf-8')
    dataframe.to_sql(table_name, con=engine, if_exists='replace', index=False)
    saveSQLtoFile(database_name,file_name)
 
+
 def loadDataset(database_name, table_name, file_name):
    loadSQLfromFile(database_name,file_name)
-   df= pd.read_sql_table(table_name, con=createEngine(database_name))
+   engine= create_engine('mysql://root:codio@localhost/'+database_name+'?charset=utf8', encoding='utf-8')
+   df= pd.read_sql_table(table_name, con=engine , if_exists='replace', index=False)
    return df
 
-def makeBarchart(df, xcol, ycol):
-   df.plot.barh(x = xcol, y = ycol )
-   plt.show()
+
+def makeScatterchart(df, xcol, ycol):
+   data = df
+   fig = px.scatter(data, x=xcol, y=ycol, color="Name", 
+                  size="Rating" ) 
+   fig.write_html('scatter.html')
+
+
+def makeBarchart(df, xcol, ycol ):
+   data= df
+   fig =px.bar(data, x = xcol, y = ycol, color= "Rating", title= "Restaurant Search Results")
+   fig.write_html('barchart.html')
+
    
-   
-   
-   
+def create_search_url(base_url, zip_code, state, term1, term2= 'None'):
+   search_url=base_url+ 'location=' + zip_code + '&' + 'term=' + term1
+   return search_url
+
+
+def api_to_json(search_url, headers):
+   r = requests.get(search_url, headers= headers)
+   print(r.status_code)
+   response_json= r.json()
+   return response_json
+
+def add_all_info_df(col_names, main_dictionary):
+   df= pd.DataFrame(columns= col_names)
+   for business in main_dictionary:  
+      df.loc[len(df.index)] = [business['name'], business['rating'], business['location']['address1'], business['location']['zip_code'],                   
+                         business['categories'][0]['alias']]
+    
+   df.sort_values(by=['Rating'], inplace=True, ascending=False) 
+   return df
+
+#Main code
+base_url='https://api.yelp.com/v3/businesses/search?'
+zip_code= collect_zip_input()
+state= collect_state_input()
+preference= collect_liked_input()
+
+search_url=create_search_url(base_url,zip_code, state, preference)
+
 api_key ='eUJ17k9QVRaQVpchITGQszLJGOhhyE52aMFr1o1AO4v52d3_-La_yyI1gj-FiGOEKFG62RrVMqh5rN7Ab12hw60MC6euNHD7mYV5sARgZS4GNYFc-g_FQH4c7c7jYHYx'
 headers = {
     'Authorization': 'Bearer %s' % api_key,
 }
 
-base_url='https://api.yelp.com/v3/businesses/search?'
-zip_code= collect_zip_input()
-state= collect_state_input()
-preference= collect_liked_input()
-# print("ZipCode: "+ zip_code + " state: " + state + " term 1: " + preference)
-
-search_url=base_url+ 'location=' + zip_code + '&' + 'term=' + preference
-
-response= requests.get(search_url, headers=headers)
-print(response.status_code)
-restaurantList =response.json()
-#  print(restaurantList)
+restaurantList = api_to_json(search_url, headers)
 
 main = restaurantList['businesses'] #inside businesses dictionary)
 
    
    
-# main_info= main[0]
-# print(main_info)
-
 col_names = ['Name','Rating', 'Location', 'ZipCode', 'Food Type']
-df = pd.DataFrame(columns = col_names)
 
-for business in main:  
-    df.loc[len(df.index)] = [business['name'], business['rating'], business['location']['address1'], business['location']['zip_code'],                   
-                         business['categories'][0]['alias']]
-    
-df.sort_values(by=['Rating'], inplace=True, ascending=False)    
-print(df)
+df= add_all_info_df(col_names, main)
+df2= df.iloc[:3]
+print(df2)
 
-database_name= 'restaurants'   
+
+#make database
+database_name= 'restaurants' 
+file_name= 'test_file.sql'
+table_name= 'restaurant_information'
+
+# saveSQLtoFile(database_name, file_name)
+# loadSQLfromFile(database_name, file_name)
+
 os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS '+database_name+';"')
 engine = create_engine('mysql://root:codio@localhost/restaurants')
-df.to_sql('restaurant_information', con = engine, if_exists='replace', index=False)
+df2.to_sql('restaurant_information', con = engine, if_exists='replace', index=False)
 
-makeBarchart(df, 'name', 'rating')
+#makeBarchart(df2, "Name", "Rating")
+#makeScatterchart(df2, "Name", "Rating")
